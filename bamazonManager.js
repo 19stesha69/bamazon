7e210713ba048d7c59a21b56df8d4b28c34effa0
SteshaMcKindle <cleodecarabas@yahoo.com>
2019-07-01 15:13:21 -0700

    complete bamazonManager.js file

--- bamazonManager.js
+++ bamazonManager.js
@@ -26,7 +26,10 @@ var connection = mysql.createConnection({
       menuOptions();
   });
 
+  //MENU OPTIONS FUNCTION
 function menuOptions() {
+    console.log("\n Bamazon Inventory System");
+    console.log(" ++++++++++++++++++++++++");
     inquirer
     .prompt([
         {
@@ -41,20 +44,175 @@ function menuOptions() {
         var menuChoice = response.menu;
         switch(menuChoice) {
         case "View Products for Sale":
-            console.log("this works!")
-            //productsForSale();
+            //console.log("this works!")
+            productsForSale();
+            break;
         case "View Low Inventory":
-            //lowInventory();
+            lowInventory();
+            break;
         case "Add to Inventory":
-            //addInventory();
+            addInventory();
+            break;
         case "Add New Product":
-            //addNewProduct();
+            addNewProduct();
+            break;
         case "Exit":
-            //exitApp();
+            exitOrStay();
+            break;
         }
     })
 }
 
+//PRODUCTS FOR SALE FUNCTION -- DISPLAYS ALL PRODUCT IN INVENTORY DATABASE
+function productsForSale() {
+    console.log("\n id  |        Product Name         |  Dept. Name |price  | qty");
+    connection.query(
+        "SELECT * FROM products",
+        function(err, answer) {
+            if (err) throw err;
+            //console.log(res[0].item_id);
+            for (var i = 0; i < answer.length; i++) {
+                var results = answer[i];
+                var itemID = results.item_id;
+                var productName = results.product_name;
+                var deptName = results.department_name;
+                var price = results.price;
+                var stockQuantity = results.stock_quantity;
+
+                console.log(" * " + itemID + " | " + productName + " | " + deptName + " | " + price + " | " + stockQuantity + "\n");
+            }
+            exitOrStay();
+        }
+    )
+}
+
+//LOW INVENTORY FUNCTION -- CHECKS 'Products' TABLE FOR STOCK LEVELS LOWER THAN 6
+function lowInventory() {
+    console.log("\n Looks like it's time to reorder!\n");
+    connection.query(
+        "SELECT * FROM products WHERE stock_quantity <= 5",
+        function(err, res) {
+            if(err) throw err;
+            for(var j = 0; j < res.length; j++) {
+                var invResults = res[j];
+                var lowInvItemID = invResults.item_id;
+                var lowInvProductName = invResults.product_name;
+                var lowInvDeptName = invResults.department_name;
+                var lowInvPrice = invResults.price;
+                var lowInvStockQuantity = invResults.stock_quantity;
+
+                console.log(" * " + lowInvItemID + " | " + lowInvProductName + " | " + lowInvDeptName + " | " + lowInvPrice + " | " + lowInvStockQuantity + "\n");
+            }
+            exitOrStay();
+        }
+    );
+}
+
+//ADD INVENTORY FUNCTION -- ALLOWS USER TO INCREASE STOCK QUANTITIES
+function addInventory () {
+    console.log("\n Increase stock quantities\n")
+    inquirer
+    .prompt ([
+        {
+            type: "input",
+            message: " Enter item ID",
+            name: "itemID"
+        },
+        {
+            type: "input",
+            message: " Enter quantity",
+            name: "quantity"
+        }
+    ])
+    .then(function(response) {
+        connection.query(
+            "SELECT * FROM products WHERE item_id = '" + response.itemID + "'",
+             function(err, res) {
+                if (err) throw err; 
+                var chosenID = response.itemID;
+                var stringNum = response.quantity;
+                var qtyNum = parseInt(stringNum, 10);
+                var newQuantity = qtyNum + res[0].stock_quantity;
+                var updateSql = "UPDATE products SET stock_quantity = ? WHERE item_id = ?";
+                var updateSqlData = [newQuantity, chosenID];
+                connection.query(updateSql, updateSqlData, (error, results, fields) => {
+                    if (error){
+                    return console.error(error.message);
+                    }
+                    //console.log('Rows affected:', results.affectedRows);
+                console.log("\n The stock quantity for item " + chosenID +  " is now " + newQuantity + "\n");
+                exitOrStay();
+                });
+             }
+        )
+    })
+}
+
+//ADD NEW PRODUCT FUNCTION -- ALLOWS USER TO ADD A NEW ROW TO THE TABLE
+function addNewProduct() {
+    console.log("\n Add new product to store\n");
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "Enter product name",
+            name: "product_name"
+        },
+        {
+            type: "input",
+            message: "Enter department name",
+            name: "department_name"
+        },
+        {
+            type: "input",
+            message: "Enter retail price",
+            name: "price"
+        },
+        {
+            type: "input",
+            message: "Enter stock quantity",
+            name: "quantity"
+        }
+    ])
+    .then(function(response) {
+        var insertProdName = response.product_name;
+        var insertDeptName = response.department_name;
+        var insertPrice = response.price;
+        var insertQuantity = response.quantity;
+        connection.query( 
+            "INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('" + insertProdName + "', '" + insertDeptName + "', '" + insertPrice + "', '" + insertQuantity + "' )",
+            function(err, res) {
+                if (err) throw err;
+                if(res.affectedRows == true) {
+                    console.log("\n New item has been successfully added\n");
+                    exitOrStay();
+                } 
+            }    
+        )   
+    })
+}
+
+//EXITORSTAY FUNCTION -- ALLOWS USER TO RETURN TO MAIN MENU OR EXIT PROGRAM 
+ function exitOrStay() {
+    inquirer
+    .prompt([
+    {
+        type: "list",
+        message: "Choose from the following: ",
+        choices: ["Return to menu options", "Exit program"],
+        name: "leave_PfS"
+    }
+    ])
+    .then(function(response) {
+        var pfsChoice = response.leave_PfS;
+        if (pfsChoice == "Return to menu options") {
+            menuOptions();
+        } else {
+            console.log("\n Thank you! \n Good bye.")
+            connection.end();
+        }
+    })
+}
 
 
 
