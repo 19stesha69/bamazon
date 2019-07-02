41a664416e5b1e9cad9422588a8450e939540e7e
SteshaMcKindle <cleodecarabas@yahoo.com>
2019-07-02 11:48:19 -0700

    add menu options to bamazonSupervisor.js

---
+++ bamazonSupervisor.js
@@ -0,0 +1,48 @@
+// DATABASE
+var mysql = require("mysql");
+
+// INQUIRER
+var inquirer = require("inquirer");
+
+//CONNECTION INFO FOR SQL DATABASE
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "BBto2941",
+    database: "bamazon_DB"
+  });
+  
+  //CONNECTS MYSQL SERVER and SQL DATABASE
+  connection.connect(function(err) {
+      if (err) throw err;
+      //console.log("connected as id " + connection.threadId + "\n");
+      superMenuOptions();
+  });
+
+  function superMenuOptions() {
+      console.log("\n Bamazon Supervisor Workspace\n");
+      inquirer
+      .prompt ([
+          {
+              type: "list",
+              message: " Please choose one of the following:",
+              choices: [ " View Product Sales by Department", " Create New Department" ],
+              name: "superMenu"
+          }
+      ])
+      .then(function(response) {
+          var superMenu = response.superMenu;
+          if (superMenu == " View Product Sales by Department") {
+            //   summarizedTable();
+          }
+        //   createNewDept();
+      })
+  }
+  
